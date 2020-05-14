724f1762641c5fbfc0dbf872ee1d11e99d871829
abdelrahmanahmed <abdelrahman.wahdan@outlook.com>
2020-05-14 17:50:29 +0200

    chore: clean

---
+++ build/precache-manifest.abcfdf40ce6b6ff60923c9a5b64ab778.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bb77467e4025d8c0f3d7af00331f4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad7d10566af4a23e37ff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39da1c9f927c506c4f7e",
+    "url": "/static/js/2.6b805406.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b805406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad7d10566af4a23e37ff",
+    "url": "/static/js/main.0a17fe6e.chunk.js"
+  },
+  {
+    "revision": "a32d12b1b3959eea1bf9",
+    "url": "/static/js/runtime-main.8b132b75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
